e966a6d0d3401fea098374a277e8d85100a08aff
Alfred-Onuada <aonuada5@gmail.com>
2025-07-25 23:56:29 +0100

    some changes

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+-- -- Autocommand to resize terminal window on open
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("lazyvim_terminal_resize", { clear = true }),
+  callback = function()
+    vim.cmd("resize 15") -- Set height to 15 lines (adjust as needed)
+  end,
+})
