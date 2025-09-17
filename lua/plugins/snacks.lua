931d80fc46dc530103ef5aa4c02ad9388234e161
Alfred-Onuada <aonuada5@gmail.com>
2025-09-17 15:59:06 +0100

    fixed explorer to always show hidden files

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,9 +1,14 @@
 return {
   {
     "folke/snacks.nvim",
-    ---@type snacks.Config
+    -- Configuration to ensure git ignored files and dot files are
+    -- always visible in file tree and search
     opts = {
       explorer = {},
+      picker = {
+        ignored = true,
+        hidden = true,
+      },
     },
   },
 }
