e26dbfdb2ff76626ca2973d3c1b9ce6c97a88993
Alfred-Onuada <aonuada5@gmail.com>
2025-10-21 07:35:26 +0100

    added support for inline git blaming

---
+++ lua/plugins/gitstuffs.lua
@@ -0,0 +1,10 @@
+return {
+  "f-person/git-blame.nvim",
+  event = "VeryLazy",
+  opts = {
+    enabled = true,
+    date_format = "%r",
+    message_template = " <author> • <date> • <summary>",
+    virtual_text_column = 0,
+  },
+}
