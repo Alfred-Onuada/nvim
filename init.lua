eb29944986a042f257323477254c362448601251
Alfred-Onuada <aonuada5@gmail.com>
2026-06-23 08:35:10 +0100

    update

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- prevent too verbose logging the lsp.log file can grow very fast - /Users/alfredonuada/.local/state/nvim/lsp.log
+vim.lsp.set_log_level("WARN")
